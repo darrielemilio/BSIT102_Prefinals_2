01fe2c5f916f4c9949a0b90aa54b68b3a8492580
darrielemilio <53457582+darrielemilio@users.noreply.github.com>
2019-10-15 09:33:15 +0800

    Update decode.java

--- decode.java
+++ decode.java
@@ -6,27 +6,23 @@
  * @author 
  * @version 1.00 2019/10/14
  */
- import java.io.File;
-import java.io.*;
-import java.util.Scanner;
+import java.util.Scanner;        // Scanner
+import java.util.Base64;         //Base64 Binary Group
+import java.io.*;                // All Input/Output Stream
 
 public class decode {
-    public static void main(String[]args) throws Exception
-		{
-			
-			System.out.println("Enter the file name :");
-			Scanner scn = new Scanner(System.in);
-			
-			File filer = new File(scn.nextLine() + ".jgh");
-			scn = new Scanner(filer);
-			
-			String lines = scn.nextLine();
-			System.out.println(lines);
-			
-		
-			
-			
-			
-		}
+  public static void main(String[] args) throws Exception {
+	Scanner in = new Scanner(System.in);
+	System.out.println("Please Enter the Filename");    //Enter the File of the Encoded Base64 Binary text
+	
+	File FileOut=new File (in.nextLine()+ ".DVN");      // Open the File of with the File extension of the Binary  
+	in = new Scanner (FileOut);                        // Make Access in Scanner to Read the File 
+	
+	byte[] decodes = Base64.getDecoder().decode(in.next());     //Base64 Decoder output from the Scanner
+	
+	String decodedStr = new String(decodes);           // Converts the decoder output from the Base64 Decoder output into a String format
+	System.out.println("\n"+decodedStr);                    // Prints the decodedStr 
+	
+	}
 }
   
